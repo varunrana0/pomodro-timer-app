c85a27f04efb6e08c7f9e8077ecd4182066cf392
Varun Rana <71178772+varunrana0@users.noreply.github.com>
2023-12-12 22:00:19 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Navigation from "./Navigation";
 import Work from "./Work";
 
 function Home() {
 	return (
 		<div className="bg-gray-900 h-full py-10">
 			<div className="max-w-4xl mx-auto h-full w-full flex flex-col items-center justify-center">
-				<Navigation />
 				<Work />
 			</div>
 		</div>
